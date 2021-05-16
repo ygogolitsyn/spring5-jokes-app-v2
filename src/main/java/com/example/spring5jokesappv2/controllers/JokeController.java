2e28c571c55c5f8edc3202665f8c44df7635287f
Yuri Gogolitsyn <ygogol@hotmail.com>
2021-05-16 17:45:56 +0100

    added controller

---
+++ src/main/java/com/example/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package com.example.spring5jokesappv2.controllers;
+
+import com.example.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke() );
+
+        return "index";
+    }
+}
